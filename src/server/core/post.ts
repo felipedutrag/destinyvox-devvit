d754d903a42f1123c0eaddac950c561fd9adca97
felipedutrag <felipedutra@outlook.com>
2026-09-11 14:59:37 -0300

    feat(webview): sync dark mode background color and add custom post styles

--- src/server/core/post.ts
+++ src/server/core/post.ts
@@ -3,5 +3,9 @@ import { reddit } from '@devvit/web/server';
 export const createPost = async () => {
   return await reddit.submitCustomPost({
     title: '✦ Unveil Your Cosmic Blueprint: Calculate Your Pythagorean Numerology Chart',
+    styles: {
+      backgroundColor: '#F7F5EFFF',
+      backgroundColorDark: '#08080CFF',
+    },
   });
 };
